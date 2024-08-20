79855ffd9a7765edcc44d848df70cca3dfdfccee
NanbanTaro <>
2024-08-20 22:16:08 +0900

    新規プロジェクト作成

---
+++ TodoApp-TCA/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  TodoApp-TCA
+//
+//  Created by NanbanTaro on 2024/08/20.
+//  
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TodoApp-TCA/TodoApp_TCAApp.swift
@@ -0,0 +1,18 @@
+//
+//  TodoApp_TCAApp.swift
+//  TodoApp-TCA
+//
+//  Created by NanbanTaro on 2024/08/20.
+//  
+//
+
+import SwiftUI
+
+@main
+struct TodoApp_TCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
